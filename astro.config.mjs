aa568b2b33677417b88f19d1cc89099c59874315
sherlock <aadit@aaditagrawal.com>
2026-04-04 14:03:41 +0530

    :octocat: mobile rendering astroconfig fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   build: {
-    format: 'file',
+    format: 'directory',
     assets: 'assets',
   },
   compressHTML: true,
