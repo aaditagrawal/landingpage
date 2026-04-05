d59b45d36f51a10a199060e26ae11df33032ea66
sherlock <aadit@aaditagrawal.com>
2026-04-05 10:26:12 +0530

    :octocat: speeeeeeeeeeeed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   build: {
-    format: 'directory',
-    assets: 'assets',
+    format: "directory",
+    assets: "assets",
   },
   compressHTML: true,
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'tap',
+    defaultStrategy: "load",
   },
 });
